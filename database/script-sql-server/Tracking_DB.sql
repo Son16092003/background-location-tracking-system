ccace898ee08aee7f8c92bef5bfbefbbb19b940d
Sơn ĐInh Trung <SON.DT13376@sinhvien.hoasen.edu.vn>
2025-12-26 23:42:31 +0700

    update script sql

--- database/script-sql-server/Tracking_DB.sql
+++ database/script-sql-server/Tracking_DB.sql
@@ -3,23 +3,29 @@ GO
 USE TrackingDB;
 GO
 
+/* =========================================================
+   TABLE: Devices
+   ========================================================= */
+CREATE TABLE dbo.Devices (
+    Id UNIQUEIDENTIFIER NOT NULL,
+    UserName NVARCHAR(100) NOT NULL,
+    Title NVARCHAR(200) NOT NULL,
+    CreatedAt DATETIME2(7) NOT NULL,
+GO
 
+/* =========================================================
+   TABLE: GPS_DeviceTracking
+   ========================================================= */
 CREATE TABLE dbo.GPS_DeviceTracking (
     Oid UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), -- Khóa chính dạng GUID
-    DeviceID NVARCHAR(1000) NOT NULL,        -- Mã thiết bị
-    Title NVARCHAR(100) NULL,                -- Tên hiển thị của thiết bị hoặc người dùng
-    Latitude FLOAT NOT NULL,                 -- Vĩ độ
-    Longitude FLOAT NOT NULL,                -- Kinh độ
-    RecordDate DATETIME NOT NULL,            -- Thời gian ghi nhận tọa độ
-    OptimisticLockField INT NULL,            -- Dùng cho cơ chế khóa lạc quan
-    GCRecord INT NULL,                       -- Cột đánh dấu soft-delete
-    UserName NVARCHAR(100) NULL              -- Tên người dùng (số điện thoại hoặc tài khoản)
+    DeviceID NVARCHAR(1000) NOT NULL,        
+    Title NVARCHAR(100) NULL,                
+    Latitude FLOAT NOT NULL,                 
+    Longitude FLOAT NOT NULL,               
+    RecordDate DATETIME NOT NULL,            
+    OptimisticLockField INT NULL,            
+    GCRecord INT NULL,                       
+    UserName NVARCHAR(100) NULL,    
+    IsOffline BIT NOT NULL          
 );
 
-
-select * from GPS_DeviceTracking
-
-SELECT * FROM GPS_DeviceTracking ORDER BY RecordDate ASC;
-
-
-truncate table GPS_DeviceTracking
